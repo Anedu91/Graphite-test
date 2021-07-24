0b4fc5e51a86d692371fa9221e4f927798941532
Angel <angel.e.diaz.b@gmail.com>
2021-07-24 09:04:31 -0500

    Runing server

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 
+//Starting the instance
 const app = express();
 
-app.listen(3000, () => console.log('3000'));
+// Using middleware
+// app.use(express.json());
+// app.use(express.urlencoded());
+
+//Cors for cross origin allowance
+app.use(cors())
+
+
+
+
+const port = 3000;
+
+const server = app.listen(port, listening)
+
+function listening(){
+  console.log('Server running')
+  console.log(`Running on localhost ${port}`)
+}
